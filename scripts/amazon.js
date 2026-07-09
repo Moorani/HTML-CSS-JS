d3b877911ec745bec62f7e8566e55d8ffab60321
Moorani <mooranisudharth@gmail.com>
2026-07-10 01:59:00 +0500

    Make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,6 +70,13 @@ document.querySelectorAll('.js-add-to-cart-btn').forEach((addToCartButton) => {
           quantity: 1
         });
       }
-      console.log(cart);
+      
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
+
+
