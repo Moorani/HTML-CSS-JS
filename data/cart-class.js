61b4b1130ffa79595ea9e7a093091c228be6d95a
Moorani <mooranisudharth@gmail.com>
2026-07-20 00:32:18 +0500

    Make LocalStorageKey private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,13 +1,13 @@
 class Cart {
-    localStorageKey;
-    cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [];
+    #localStorageKey;
+    cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)) || [];
 
     constructor (localStorageKey) {
-        this.localStorageKey = localStorageKey;
+        this.#localStorageKey = localStorageKey;
     }
 
     saveToStorage() {
-      localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+      localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
     }
 
 
